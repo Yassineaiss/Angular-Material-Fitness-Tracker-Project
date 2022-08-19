aaf30340900aa74d2ae2d28b2a1a7cab1738832e
Yassine Aissaoui <Yassaiss@gmail.com>
2022-08-19 17:07:34 +0200

    Controlling the Layout with @angularflex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -28,7 +28,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,15 @@
 import { ModuleWithProviders, NgModule, Type } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 
 
 const materials: any[] | Type<any> | ModuleWithProviders<{}>=[
   MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule
+
 ];
 
 @NgModule({
